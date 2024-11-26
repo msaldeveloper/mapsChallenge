fa2f8721c3a51ce6c57bc8e058e704543b0e9f3b
msaldeveloper <msalda53@gmail.com>
2024-11-26 16:27:19 -0600

    add project

---
+++ mapsChallenge/mapsChallenge/mapsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsChallengeApp.swift
+//  mapsChallenge
+//
+//  Created by Mario Alberto Saldaña Martínez on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mapsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
