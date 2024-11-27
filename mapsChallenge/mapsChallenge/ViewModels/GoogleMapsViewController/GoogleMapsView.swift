df400cf7856cf1eba6fe5d6655a152325a5a4def
msaldeveloper <msalda53@gmail.com>
2024-11-27 08:34:53 -0600

    add marker

--- mapsChallenge/mapsChallenge/ViewModels/GoogleMapsViewController/GoogleMapsView.swift
+++ mapsChallenge/mapsChallenge/ViewModels/GoogleMapsViewController/GoogleMapsView.swift
@@ -14,12 +14,16 @@ struct GoogleMapView: UIViewRepresentable {
     var zoom: Float
 
     func makeUIView(context: Context) -> GMSMapView {
+        let objectCamera = GMSMarker()
         let camera = GMSCameraPosition.camera(
             withLatitude: latitude,
             longitude: longitude,
             zoom: zoom
         )
+        objectCamera.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        objectCamera.map = mapView
         return mapView
     }
 
